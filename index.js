7fb13ca425f9c9a0add8efd3e20e3db2fe773e11
Dave Townsend <dtownsend@oxymoronical.com>
2021-08-10 12:06:21 +0100

    Switch to prettier formatting.

--- index.js
+++ index.js
@@ -13,33 +13,17 @@ module.exports = {
         "plugin:import/warnings",
         "plugin:import/recommended",
         "plugin:import/typescript",
+        "prettier",
       ],
       "rules": {
-        "array-bracket-newline": ["warn", "consistent"],
-        "array-bracket-spacing": "warn",
         "array-callback-return": "error",
-        "array-element-newline": ["warn", "consistent"],
-        "arrow-parens": ["warn", "as-needed"],
-        "arrow-spacing": "warn",
         "@typescript-eslint/array-type": "error",
         "@typescript-eslint/ban-ts-comment": "off",
-        "block-spacing": "warn",
-        "brace-style": "off",
-        "@typescript-eslint/brace-style": "warn",
-        "comma-dangle": "off",
-        "@typescript-eslint/comma-dangle": ["warn", "always-multiline"],
-        "comma-spacing": "off",
-        "@typescript-eslint/comma-spacing": "warn",
-        "comma-style": "warn",
         "consistent-return": "warn",
         "@typescript-eslint/consistent-type-assertions": "warn",
         "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
         "@typescript-eslint/consistent-type-imports": "warn",
         "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/func-call-spacing": "warn",
-        "function-call-argument-newline": ["warn", "consistent"],
-        "function-paren-newline": ["warn", "multiline-arguments"],
-        "generator-star-spacing": "warn",
         "import/extensions": ["warn", "never"],
         "import/first": "warn",
         "import/newline-after-import": "warn",
@@ -57,16 +41,8 @@ module.exports = {
             "caseInsensitive": true,
           }
         }],
-        "indent": "off",
-        "@typescript-eslint/indent": ["warn", 2, {
-          "ignoredNodes": ["TSTypeParameterInstantiation"],
-          "SwitchCase": 1,
-        }],
-        "key-spacing": "warn",
-        "keyword-spacing": "warn",
         "linebreak-style": "warn",
         "max-len": ["warn", 100],
-        "multiline-ternary": ["warn", "always-multiline"],
         "@typescript-eslint/naming-convention": ["warn",
           {
             "selector": "default",
@@ -168,12 +144,10 @@ module.exports = {
         "@typescript-eslint/no-dupe-class-members": "warn",
         "no-duplicate-imports": "off",
         "@typescript-eslint/no-duplicate-imports": "warn",
-        "@typescript-eslint/no-extra-parens": "warn",
-        "@typescript-eslint/no-extra-semi": "error",
+        "@typescript-eslint/no-explicit-any": "off",
         "no-fallthrough": "warn",
         "no-lone-blocks": "warn",
         "no-lonely-if": "warn",
-        "no-multi-spaces": "warn",
         "no-multiple-empty-lines": ["warn", {
           "max": 1,
           "maxBOF": 0,
@@ -181,8 +155,8 @@ module.exports = {
         }],
         "no-nested-ternary": "warn",
         "no-new-wrappers": "error",
+        "@typescript-eslint/no-non-null-assertion": "off",
         "no-throw-literal": "error",
-        "no-trailing-spaces": "warn",
         "@typescript-eslint/no-unnecessary-condition": "error",
         "@typescript-eslint/no-unnecessary-type-constraint": "warn",
         "@typescript-eslint/no-unsafe-assignment": "off",
@@ -200,63 +174,22 @@ module.exports = {
           "noUselessIndex": true,
         }],
         "no-whitespace-before-property": "warn",
-        "object-curly-newline": ["warn", {
-          "multiline": true,
-          "consistent": true,
-        }],
-        "object-curly-spacing": ["warn", "always"],
-        "object-property-newline": ["warn", {
-          "allowAllPropertiesOnSameLine": true,
-        }],
         "operator-assignment": "warn",
-        "operator-linebreak": ["warn", "after", {
-          "overrides": {
-            "?": "before",
-            ":": "before",
-          },
-        }],
-        "padded-blocks": ["warn", "never"],
         "prefer-const": "off",
         "@typescript-eslint/prefer-nullish-coalescing": "warn",
         "@typescript-eslint/prefer-optional-chain": "warn",
         "prefer-rest-params": "warn",
         "prefer-spread": "warn",
-        "quote-props": ["warn", "consistent-as-needed"],
-        "quotes": "off",
-        "@typescript-eslint/quotes": "warn",
         "require-atomic-updates": "off",
         "@typescript-eslint/require-await": "off",
         "rest-spread-spacing": "warn",
         "@typescript-eslint/restrict-template-expressions": "off",
-        "semi-spacing": "warn",
-        "semi": "off",
-        "@typescript-eslint/semi": "error",
-        "space-before-blocks": "warn",
-        "@typescript-eslint/space-before-function-paren": ["warn", {
-          "named": "never",
-          "anonymous": "never",
-          "asyncArrow": "always",
-        }],
-        "space-in-parens": "warn",
-        "space-infix-ops": "off",
-        "@typescript-eslint/space-infix-ops": "warn",
-        "space-unary-ops": ["warn", {
-          "words": true,
-          "nonwords": false,
-        }],
         "spaced-comment": ["warn", "always", {
           "block": {
             "balanced": true,
           },
         }],
-        "switch-colon-spacing": "warn",
-        "template-curly-spacing": "warn",
-        "yield-star-spacing": "warn",
-      },
-      "settings": {
-        "import/parsers": {
-          "@typescript-eslint/parser": [".js", ".jsx", ".ts", ".tsx"]
-        },
+        "@typescript-eslint/unbound-method": "off",
       },
       "overrides": [{
         // enable these rules specifically for TypeScript files
@@ -291,8 +224,7 @@ module.exports = {
         },
         "settings": {
           "import/resolver": {
-            // use <root>/tsconfig.json
-            "ts": {
+            "typescript": {
               "alwaysTryTypes": true,
             },
           },
@@ -315,52 +247,28 @@ module.exports = {
         "env": {
           "jest": true,
         },
-        "rules": {
-          "@typescript-eslint/no-non-null-assertion": "off",
-          "@typescript-eslint/no-explicit-any": "off",
-          "@typescript-eslint/unbound-method": "off",
-        },
       }]
     },
     "react": {
       "plugins": [
         "react",
+        "react-hooks",
       ],
       "extends": [
         "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
       ],
       "rules": {
         "react/button-has-type": "warn",
         "react/jsx-boolean-value": ["warn", "always"],
-        "react/jsx-child-element-spacing": "warn",
-        "react/jsx-closing-bracket-location": ["warn", "line-aligned"],
         "react/jsx-curly-brace-presence": ["warn", "never"],
-        "react/jsx-curly-newline": ["warn", {
-          "multiline": "require",
-          "singleline": "consistent",
-        }],
-        "react/jsx-curly-spacing": "warn",
-        "react/jsx-equals-spacing": "warn",
         "react/jsx-filename-extension": ["warn", {
           "extensions": [".tsx", ".jsx"],
         }],
-        "react/jsx-first-prop-new-line": ["warn", "multiline"],
-        "react/jsx-fragments": ["warn", "element"],
-        "react/jsx-indent": ["warn", 2],
-        "react/jsx-indent-props": ["warn", "first"],
-        "react/jsx-max-props-per-line": ["warn", {
-          "maximum": 1,
-          "when": "multiline",
-        }],
+        "react/jsx-fragments": ["warn", "syntax"],
         "react/jsx-no-bind": "warn",
         "react/jsx-no-useless-fragment": "warn",
         "react/jsx-pascal-case": "warn",
-        "react/jsx-tag-spacing": ["warn", {
-          "closingSlash": "never",
-          "beforeSelfClosing": "never",
-          "afterOpening": "never",
-          "beforeClosing": "never"
-        }],
         "react/no-access-state-in-setstate": "warn",
         "react/no-array-index-key": "warn",
         "react/no-danger": "warn",
@@ -374,6 +282,7 @@ module.exports = {
         "react/no-will-update-set-state": "error",
         "react/prefer-es6-class": "error",
         "react/prop-types": "off",
+        "react/react-in-jsx-scope": "off",
         "react/require-optimization": "warn",
         "react/self-closing-comp": "warn",
         "react/style-prop-object": "error",
